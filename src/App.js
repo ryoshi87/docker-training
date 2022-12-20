4141c9d34db4bcc2800d6f03d46780bda80fb2b1
Daniel Fischer <daniel.fischer@optano.com>
2022-12-20 16:39:34 +0100

    next release test

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          I was changed from VSCode!
+          This is the next release!
         </p>
         <a
           className="App-link"
